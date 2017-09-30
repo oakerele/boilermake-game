8c62e1bbf08f924cffba8c4a79007514fd5b94fa
Conflagrationator <willmarrujo@gmail.com>
2017-09-30 13:01:24 -0400

    game js & mongodb loaded

--- server/server.js
+++ server/server.js
@@ -1,21 +1,35 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require("express")
+var app = express()
+var http = require("http").Server(app)
+var io = require("socket.io")(http)
+var game = require("./game.js")
+var mongodb = require("mongodb")
 
-app.use(express.static(__dirname + '/pages'));
+var port = process.env.PORT || 3001
 
-io.on('connection', function(socket) {
-    console.log('user connected.');
-    socket.on('disconnect', function() {
-        console.log('user disconnected.');
-    });
+////////////////////////////////////////////////////////////////////////////////
+// STATIC HOSTING
+////////////////////////////////////////////////////////////////////////////////
 
-    socket.on('command', function(msg) {
-        io.emit('action', {command : msg});
-    });
-});
+app.use(express.static(__dirname + "/pages"))
 
-http.listen(3000, function(){
-     console.log('listening on *:3000');
-});
+////////////////////////////////////////////////////////////////////////////////
+// SOCKET CONNECTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+io.on("connection", function(socket) {
+    console.log("user connected.")
+    socket.on("disconnect", function() {
+        console.log("user disconnected.")
+    })
+    
+    socket.on("command", function(msg) {
+        io.emit("action", {"command" : msg})
+    })
+})
+
+////////////////////////////////////////////////////////////////////////////////
+// RUN SERVER
+////////////////////////////////////////////////////////////////////////////////
+
+http.listen(port, () => { console.log("Server running on port " + port + " at "  + (new Date).toUTCString()) })
